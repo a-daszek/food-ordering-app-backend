6885fdc40dac0778b308642fce87c5cbe2011223
Anna Daszyńska <anna.daszynska@protonmail.com>
2024-03-29 19:50:31 +0100

    adding user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -21,3 +21,33 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Podaj kraj"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName")
+    .notEmpty()
+    .withMessage("Nazwa restauracji jest wymagana"),
+  body("city").notEmpty().withMessage("Nazwa miasta jest wymagana"),
+  body("country").notEmpty().withMessage("Kraj jest wymagany"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Cena za dowóz jest wymagana"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Czas oczekiwania musi być wartością dodatnią"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Dania muszą być tablicą")
+    .not()
+    .isEmpty()
+    .withMessage("Tabliuca dań nie może być pusta"),
+  body("menuItems").isArray().withMessage("Menu musi być tablicą"),
+  body("menuItems.*.name")
+    .notEmpty()
+    .withMessage("Nazwa pozycji w menu jest wymagana"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage(
+      "Cena pozycji w menu jest wymagana i musi być wartością dodatnią"
+    ),
+  handleValidationErrors,
+];
