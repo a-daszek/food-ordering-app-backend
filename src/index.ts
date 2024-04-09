4c09d19983fc0674d96a05874ae6129a6f375c8a
Anna Daszyńska <anna.daszynska@protonmail.com>
2024-04-09 19:11:03 +0200

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -34,6 +35,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
   console.log("Serwer działa na localhost:7000");
